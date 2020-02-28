02add58ea3b726db9cf7f8bc951187063659adb4
merchandayzer <60540406+merchandayzer@users.noreply.github.com>
2020-02-28 10:16:12 +0300

    third commit

--- project1.rb
+++ project1.rb
@@ -1,3 +1,6 @@
+#Задание 1
+
+
 a = gets.to_f
 b = gets.to_f
 c = gets.to_f
@@ -5,23 +8,23 @@ c = gets.to_f
 D = b * b - 4 * a * c
 
 if D > 0
-	x1 = (- b + Math.tan(D)) / 2 * a
-	x2 = (- b - Math.tan(D)) / 2 * a
-	puts
-	puts "D = " + D.to_s
-	puts "x1 = " + x1.to_s
-	puts "x2 = " + x2.to_s
+  x1 = (- b + Math.tan(D)) / 2 * a
+  x2 = (- b - Math.tan(D)) / 2 * a
+  puts
+  puts "D = " + D.to_s
+  puts "x1 = " + x1.to_s
+  puts "x2 = " + x2.to_s
 end
 
 if D == 0
-	x1 = - b / 2 * a
-	puts
-	puts "D = " + D.to_s
-	puts "x1 = x2 = " + x1.to_s
+  x1 = - b / 2 * a
+  puts
+  puts "D = " + D.to_s
+  puts "x1 = x2 = " + x1.to_s
 end
 
 if D < 0
-	puts
-	puts "D = " + D.to_s
-	puts "Корней нет"
+  puts
+  puts "D = " + D.to_s
+  puts "No roots"
 end
--- project2.rb
+++ project2.rb
@@ -9,28 +9,28 @@ sum1 = 0
 sum2 = 0
 
 loop do
-	puts "Enter goods name"
-	goods_names = STDIN.gets.chomp.downcase
-	if (goods_names == "stop")
-		break
-	else
-		puts "Enter the price for one goods "
-		price = STDIN.gets.chomp.downcase
-		puts "Enter amout of goods "
-		amout = STDIN.gets.chomp.downcase
-		purchases[goods_names] = {amout => price}
-	end
+  puts "Enter goods name"
+  goods_names = STDIN.gets.chomp.downcase
+  if (goods_names == "stop")
+    break
+  else
+    puts "Enter the price for one goods "
+    price = STDIN.gets.chomp.downcase
+    puts "Enter amout of goods "
+    amout = STDIN.gets.chomp.downcase
+    purchases[goods_names] = {amout => price}
+  end
 end
 
 purchases.each do |name, value|
-	puts
-	puts "Name: #{name}"
-		value.each do |k, v|
-		puts "Price: #{v}; Amount: #{k}"
-		sum1 = v.to_f * k.to_f
-		sum2 += v.to_f * k.to_f
-		puts "Sum for this goods: #{sum1}"
-	end
+  puts
+  puts "Name: #{name}"
+    value.each do |k, v|
+    puts "Price: #{v}; Amount: #{k}"
+    sum1 = v.to_f * k.to_f
+    sum2 += v.to_f * k.to_f
+    puts "Sum for this goods: #{sum1}"
+  end
 end
 
 puts
